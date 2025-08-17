ad740f2868e5c02fbe70956a2279ecc26bbea5c4
randomusert <eetupajamaki@gmail.com>
2025-08-17 13:57:03 +0300

    build works

--- src/liblia/liblia.c
+++ src/liblia/liblia.c
@@ -1,15 +1,10 @@
 #include "liblia.h"
-#include "../liblinux/liblinux.h"
-#include "../libwin/libwin.h"
 
+//liblia init function
+// This function initializes the liblia library.
 void liblia_init(void) {
     // Initialization code for the library can be added here.
     // For now, we just print a message to indicate that the library has been initialized.
-    #if defined(__linux__)
-    liblinux_init();
-    #elif defined(__WIN32)
-    libwin_init();
-    #endif
     
     printf("liblia initialized.\n");
 }
--- src/main.c
+++ src/main.c
@@ -1,9 +1,21 @@
 #include "liblia/liblia.h"
 
-int main(void) {
-    // Initialize the library
-    liblia_init();
+#ifdef WIN32
+#include "libwin/libwin.h"
+#elif defined(__linux__)
+#include "liblinux/liblinux.h"
+#endif
 
+int main(void) {
+    // Platform-specific initialization
+#ifdef WIN32
+    libwin_init();
+#elif defined(__linux__)
+    liblinux_init();
+#endif
+
+    // Common library init
+    liblia_init();
 
     return 0;
 }
